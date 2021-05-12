59b4491baf40a2e47713981f1d6a1a72ea191eb4
DIGANTA CHAUDHURI <DIGANTACHAUDHURI03@GMAIL.COM>
2021-05-12 23:42:10 +0530

    Login with Anonomusly Done

--- lib/App/LandingPage.dart
+++ lib/App/LandingPage.dart
@@ -10,10 +10,23 @@ class LandingPage extends StatefulWidget {
 
 class _LandingPageState extends State<LandingPage> {
   User _user;
+  /*init is used to login the user after also user close the app*/
+  void initState() {
+    //init state is used to state inisilization when our landing page is inisilizd
+    super.initState();
+    //below line just triggers when a user is logged in and restats the app. in that time
+    //the LandingPage will automattically rebuild with out the signin page.
+    _updateUser(FirebaseAuth.instance.currentUser);
+  }
+
   void _updateUser(User user) {
     //Here we call setState to change state because we need to go to the
     // new page after sign in
-    print('UID : ${user.uid}');
+
+    /*On the landing page line 16 I am printing print('UID : ${user.uid}'); 
+    but because you signed out and passing null here onSignOut: () => _updateUser(null),
+     the user is set to null and therefore it does not have a uid property.*/
+    print('UID : ${user?.uid}');
     setState(
       () {
         _user = user;
